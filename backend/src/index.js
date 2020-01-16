e6eb0dcfc35e00d58d7b4d0f76177bc415a6da27
Lucas Hiago <lucashiago63@gmail.com>
2020-01-16 09:43:09 -0300

    Add cors package

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://lucas_omnistack:omnistack@cluster0-m824n.mongodb
   useFindAndModify: false,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
