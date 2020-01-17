7f539fe361e4c055a0359ba4741127cd97a43671
Lucas Hiago <lucashiago63@gmail.com>
2020-01-17 00:01:35 -0300

    Fix bug in mongo search location

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -17,7 +17,7 @@ module.exports = {
             type: 'Point',
             coordinates: [longitude, latitude],
           },
-          $maxDIstance: 10000.
+          $maxDistance: 10000.
         },
       },
     });
