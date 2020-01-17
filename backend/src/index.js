193c14039138b4c5da467839d1968872f4a26b6d
Lucas Hiago <lucashiago63@gmail.com>
2020-01-17 11:30:44 -0300

    Finish communication between mobile and back to show new devs

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+const http = require('http');
+const { setupWebsocket } = require ('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://lucas_omnistack:omnistack@cluster0-m824n.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -16,4 +21,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,11 +5,16 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 export default function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
   const [currentRegion, setCurrentRegion] = useState(null);
   const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]));
+  }, [devs]);
 
   useEffect(() => {
     async function loadInitialPosition() {
@@ -34,6 +39,18 @@ export default function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  function setupWebsocket() {
+    disconnect();
+
+    const { latitude, longitude } = currentRegion;
+
+    connect(
+      latitude,
+      longitude,
+      techs
+    );
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
 
@@ -46,9 +63,9 @@ export default function Main({ navigation }) {
       }
     });
 
-    console.log(response.data);
-
     setDevs(response.data.devs);
+
+    setupWebsocket();
   }
 
   function handleRegionChanged(region) {
@@ -61,51 +78,43 @@ export default function Main({ navigation }) {
 
   return(
     <>
-    <KeyboardAvoidingView
-      behavior={Platform.select({ android: 'height', ios: 'padding' })}
-      style={{ flex: 1 }}>
-      <MapView
-        onRegionChange={handleRegionChanged}
-        initialRegion={currentRegion} 
-        style={styles.map}
-      >
-        {devs.map(dev => (
-          <Marker
-            key={dev._id}
-            coordinate={{
-              longitude: dev.location.coordinates[0], 
-              latitude: dev.location.coordinates[1]
-            }}
-          >
-            <Image style={styles.avatar} source={{ uri: dev.avatar_url }}/>
-              <Callout onPress={() => {
-                navigation.navigate('Profile', { github_username: dev.github_username });
-              }}>
-                <View style={styles.callout}>
-                  <Text style={styles.devName}>{dev.name}</Text>
-                  <Text style={styles.devBio}>{dev.bio}</Text>
-                  <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
-                </View>
-              </Callout>
-          </Marker>
-        ))}
-      </MapView>
-      <View style={styles.searchForm}>
-        <TextInput 
-          style={styles.searchInput}
-          placeholder="Buscar devs por techs..."
-          placeholderTextColor="#999"
-          autoCapitalize="words"
-          autoCorrect={false}
-          value={techs}
-          onChangeText={text => setTechs(text)}
-        />
+    <MapView onRegionChange={handleRegionChanged} initialRegion={currentRegion} style={styles.map}>
+      {devs.map(dev => (
+        <Marker
+          key={dev._id}
+          coordinate={{
+            longitude: dev.location.coordinates[0], 
+            latitude: dev.location.coordinates[1]
+          }}
+        >
+          <Image style={styles.avatar} source={{ uri: dev.avatar_url }}/>
+            <Callout onPress={() => {
+              navigation.navigate('Profile', { github_username: dev.github_username });
+            }}>
+              <View style={styles.callout}>
+                <Text style={styles.devName}>{dev.name}</Text>
+                <Text style={styles.devBio}>{dev.bio}</Text>
+                <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
+              </View>
+            </Callout>
+        </Marker>
+      ))}
+    </MapView>
+    <View style={styles.searchForm}>
+      <TextInput 
+        style={styles.searchInput}
+        placeholder="Buscar devs por techs..."
+        placeholderTextColor="#999"
+        autoCapitalize="words"
+        autoCorrect={false}
+        value={techs}
+        onChangeText={text => setTechs(text)}
+      />
 
-        <TouchableOpacity onPress={loadDevs}style={styles.loadButton}>
-          <MaterialIcons name="my-location" size={20} color="#FFF"/>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
+      <TouchableOpacity onPress={loadDevs}style={styles.loadButton}>
+        <MaterialIcons name="my-location" size={20} color="#FFF"/>
+      </TouchableOpacity>
+    </View>
     </>
   );
 }
